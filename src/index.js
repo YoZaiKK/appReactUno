bed1bd3d86e2aa8f279128949389abbe116bb2a7
YoZaiKK <memocargo@hotmail.com>
2022-08-26 11:30:39 -0500

    primer componente creado, no hay utilidad 52

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<h1>Hello world!!</h1>)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  const condicional = false;
+  return <h1>{condicional ? "Hello my people :D" : "Negativo pareja"}</h1>;
+}
+root.render(
+  <div>
+    <Greeting />
+    <Greeting />
+  </div>
+);
