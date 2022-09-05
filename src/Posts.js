72bf60bcb7df01de03c4704a657e93baedd67979
Guillermo <memocargo@hotmail.com>
2022-09-04 20:34:32 -0500

    un commit con el inicio del consimo de un API

--- src/Posts.js
+++ src/Posts.js
@@ -1,6 +1,26 @@
+import { Gi3DHammer } from "react-icons/gi";
+
 export const Posts = () => {
-  return <ul>
-    <li>Titulo de una publicacion</li>
-    <li>Titulo de otra publicacion jajja</li>
-  </ul>
-}
+	return (
+		<>
+			<button
+				onClick={() => {
+					fetch("https://rickandmortyapi.com/api/character")
+						.then((response) => response.json())
+						.then((data) => {
+              // data.results.map(chraracter =>{
+              //   return <>
+              //     <h1>{chraracter.name}</h1>
+              //   </>
+              // })
+              console.log(data)
+            })
+						.catch((err) => console.log(err));
+				}}
+			>
+				<Gi3DHammer />
+				Traer Datos
+			</button>
+		</>
+	);
+};
