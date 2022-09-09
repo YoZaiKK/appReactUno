f503702e2b29a28476452feeb495e97b97c5d044
Guillermo <memocargo@hotmail.com>
2022-09-08 20:13:14 -0500

    primer hook

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; 
-import {Posts } from "./Posts";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function Contador() {
+	const [mensaje, setMensaje] = useState("");
+	const [texto, setTexto] = useState("");
+
+	return (
+		<>
+			<p>Hola{mensaje !== "" ? " " + mensaje : "!"}</p>
+			<input onChange={(e) => setMensaje(e.target.value)} />
+			<button
+				onClick={() => {
+					setTexto(mensaje);
+				}}
+			>
+				Save
+			</button>
+			<p>{texto !== "" ? " " + texto : ""}</p>
+		</>
+	);
+}
 
 root.render(
-  <> 
-    <Posts/>
-  </>
+	<>
+		<Contador />
+		<Posts />
+	</>
 );
